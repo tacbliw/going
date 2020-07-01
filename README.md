3a5a8a4e0df0e13e459e5bd1fd1f6a6ae64c24ea
wildcat <letranhaitung@gmail.com>
2020-07-02 01:48:50 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+#going
+Take a list of hostnames and check connectivity by pinging each host.
+##Installation
+```
+$ go get github.com/tacbliw/going
+```
+
+##Usage
+```
+$ going /path/to/host/list
+```
---
+++ main.go
@@ -0,0 +1,88 @@
+package going
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"io"
+	"net"
+	"os"
+	"sync"
+	"time"
+
+	"github.com/sparrc/go-ping"
+)
+
+func main() {
+	flag.Parse()
+
+	var input io.Reader
+
+	if flag.NArg() > 0 {
+		file, err := os.Open(flag.Arg(0))
+		if err != nil {
+			fmt.Printf("Failed to open input file: %s\n", err)
+			os.Exit(1)
+		}
+		input = file
+	}
+
+	sc := bufio.NewScanner(input)
+
+	urls := make(chan string, 128)
+	concurrency := 10
+	var wg sync.WaitGroup
+	wg.Add(concurrency)
+
+	for i := 0; i < concurrency; i++ {
+		go func() {
+			defer wg.Done()
+			for url := range urls {
+				if !resolves(url) {
+					// fmt.Printf("Cannot resolve: %s\n", url)
+					continue
+				}
+				tryPing(url)
+			}
+		}()
+	}
+
+	for sc.Scan() {
+		urls <- sc.Text()
+	}
+	close(urls)
+
+	if sc.Err() != nil {
+		fmt.Printf("error: %s\n", sc.Err())
+	}
+
+	wg.Wait()
+}
+
+func resolves(u string) bool {
+	_, err := net.LookupIP(u)
+	if err != nil {
+		return false
+	}
+	return true
+}
+
+func tryPing(url string) {
+	pinger, err := ping.NewPinger(url)
+	if err != nil {
+		fmt.Printf("Cannot initialize Pinger.\n")
+		return
+	}
+	pinger.Count = 1
+	pinger.OnRecv = func(pkt *ping.Packet) {
+		fmt.Printf("%s\n", url)
+	}
+	go func(p *ping.Pinger) {
+		time.Sleep(500 * time.Millisecond)
+		if pinger.PacketsRecv == 1 {
+			return
+		}
+		pinger.Stop()
+	}(pinger)
+	pinger.Run()
+}
